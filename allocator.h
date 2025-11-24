a5287f19f205fafa93baa9f9db5d497344e4d18f
Rory Poulter <rorypoulter2@gmail.com>
2025-11-24 15:36:03 +0000

    Fix issue with inconsistent mm_init return types (#3)

--- allocator.h
+++ allocator.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 
-void mm_init(uint8_t *heap, size_t heap_size);
+int mm_init(uint8_t *heap, size_t heap_size);
 void *mm_malloc(size_t size);
 int mm_read(void *ptr, size_t offset, void *buf, size_t len);
 int mm_write(void *ptr, size_t offset, const void *src, size_t len);
